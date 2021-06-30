24c5e9731e090e73be1b061e2833d5ed460bc0aa
nianc <2840083875@qq.com>
2021-06-30 21:33:24 +0800

    restructured

--- src/tree/case_gen.rs
+++ src/tree/case_gen.rs
@@ -1,39 +1,151 @@
-// //! 2021.6.30; This module is not finished yet.
-//
-// use super::TreeHandle;
-// use std::collections::HashSet;
-// use std::time::Duration;
-//
-// pub type Bone = Vec<bool>;
-//
-// #[derive(Debug)]
+//! 2021.6.30; This module is not finished yet.
+
+use super::TreeHandle;
+
+pub enum GenError {
+    WillOverflow,
+    IllegalRate,
+}
+
+pub type Bone = Vec<bool>;
+pub type Result<T> = std::result::Result<T, GenError>;
+
+pub struct BoneBuilder;
+
+pub enum Direction {
+    OneWayLeft,
+    OneWayRight,
+    Random,
+}
+
+impl BoneBuilder {
+    pub fn fixed_len_one(tree_len: u32) -> Bone {
+        unimplemented!()
+    }
+
+    pub fn fixed_len_drained(tree_len: u32) -> Result<Vec<Bone>> {
+        unimplemented!()
+    }
+
+    pub fn fixed_len_drained_lte(tree_len_ub: u32) -> Result<Vec<Bone>> {
+        unimplemented!()
+    }
+
+    pub fn fixed_len_rate(rate: f64) -> Result<Vec<Bone>> {
+        unimplemented!()
+    }
+
+    pub fn fixed_len_nodup(tree_len: u32, tree_num: u32) -> Result<Vec<Bone>> {
+        unimplemented!()
+    }
+
+    pub fn linked_list(tree_len: u32, d: Direction) -> Bone {
+        unimplemented!()
+    }
+
+    pub fn linked_list_custom(tree_len: u32, random_len: u32) -> (Bone, Bone, Vec<Bone>) {
+        unimplemented!()
+    }
+
+    pub fn heap(tree_len: u32) -> Bone {
+        unimplemented!()
+    }
+
+    pub fn heap_lte(tree_len_ub: u32) -> Vec<Bone> {
+        unimplemented!()
+    }
+
+    pub fn pyramid(height: u8) -> Result<Bone> {
+        unimplemented!()
+    }
+
+    pub fn pyramid_lte(height_ub: u8) -> Result<Bone> {
+        unimplemented!()
+    }
+
+    pub fn complete(nonleaf_len: u32) -> Result<Bone> {
+        unimplemented!()
+    }
+}
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+pub enum Distribution {
+    Sorted,
+    UniqueSorted,
+    Uni(Option<i32>),
+    Random,
+}
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+pub enum SeqRange {
+    Positive,
+    NonNegative,
+    NonPositive,
+    Negative,
+    Z,
+}
+
+#[derive(Debug)]
+struct ValueRenderer<'a> {
+    data: &'a [Bone],
+    pub dist: Distribution,
+    pub r: SeqRange,
+}
+
+impl<'a> ValueRenderer<'a> {
+    #[inline]
+    pub fn new(data: &'a [Bone]) -> Self {
+        Self {
+            data,
+            dist: Distribution::Random,
+            r: SeqRange::Z,
+        }
+    }
+
+    #[inline]
+    pub fn from(data: &'a [Bone], dist: Distribution, r: SeqRange) -> Self {
+        Self { data, dist, r }
+    }
+
+    #[inline]
+    pub fn reset(&'a mut self, b: &'a [Bone]) {
+        self.data = b;
+    }
+
+    pub fn get(&self) -> Vec<TreeHandle> {
+        unimplemented!()
+    }
+}
+
+// #[derive(Debug, Clone)]
 // pub enum Subset {
 //     Default,
-//     FixedLen(u32),
 //     // of len *
-//     FixedHeight(u32),
+//     FixedLen(u32),
 //     // of height *
+//     FixedHeight(u32),
+//     // of height no more than *
 //     SingleList(u32),
 //     // of height no more than *
 //     FullShape(u8),
-//     // of height no more than *
+//     // of len no more than *
 //     HeapShape(u32),
 //     // of len no more than *
 //     CompleteShape(u32),
-//     // of len no more than *
-//     Shape(Vec<Bone>), // of shapes specified in *
+//     // of shapes specified in *
+//     Shape(Vec<Bone>),
 // }
 //
-// #[derive(Debug)]
+// #[derive(Debug, Copy, Clone)]
 // pub enum StopWhen {
 //     StructureDrained,
-//     TriedTimes(u64),
+//     TriedTimes(u32),
 //     TimesUp(Duration),
 //     GotNums(u32),
 //     GotRate(f64),
 // }
 //
-// #[derive(Debug)]
+// #[derive(Debug, Copy, Clone)]
 // pub enum ValueDistribution {
 //     Sorted,
 //     UniqueSorted,
@@ -51,7 +163,7 @@
 // }
 //
 // #[derive(Debug, Eq, PartialEq)]
-// pub enum GenError {}
+
 //
 // impl GenOptions {
 //     pub fn new() -> GenOptions {
@@ -98,12 +210,18 @@
 //         unimplemented!()
 //     }
 //
-//     /* priv */ unsafe fn gen_unchecked(self) -> Vec<TreeHandle> {
+//     /* priv */ unsafe fn gen_everything_unchecked(self) -> Vec<TreeHandle> {
 //         unimplemented!()
 //     }
 //
 //     pub fn test(&self) -> Result<(), GenError> {
-//         unimplemented!()
+//         if let Subset::FixedHeight(u) = self.subset{
+//             if n > 32{
+//                 return Err(GenError::WillOverflowU32(self.subset.clone(), self.stop_when))
+//             }
+//         }
+//
+//         Ok(())
 //     }
 // }
 //
@@ -122,15 +240,15 @@
 //         unimplemented!()
 //     }
 //
-//     pub fn run(self) -> Result<FinalFilter, GenError> {
+//     pub fn run(self) -> Result<TreeSetFilter, GenError> {
 //         let mut v = vec![2, 3, 4];
 //         unimplemented!()
 //     }
 // }
 //
-// pub struct FinalFilter(Vec<TreeHandle>);
+// pub struct TreeSetFilter(Vec<TreeHandle>);
 //
-// impl FinalFilter {
+// impl TreeSetFilter {
 //     #[inline]
 //     pub fn sort_by_cached_key_and_then_dedup_by<K, KeyCluster, Dedup>
 //     (&mut self, f: KeyCluster, mut d: Dedup)
@@ -146,24 +264,17 @@
 //     pub fn get(self) -> Vec<TreeHandle> { self.0 }
 // }
 //
-// pub trait BoneRenderer {
+// pub trait Renderer {
+//     #[inline]
+//     fn render(t: TreeHandle) -> String;
+// }
+//
+// pub struct LeetcodeTreeFormatRenderer;
+//
+// impl Renderer for LeetcodeTreeFormatRenderer {
+//     #[inline]
 //     fn render(t: TreeHandle) -> String {
-//         unimplemented!()
+//         super::T(t).to_leetcode_raw()
 //     }
-// }
-//
-// pub struct LeetcodeRenderer;
-//
-// impl BoneRenderer for LeetcodeRenderer {}
-//
-// #[inline]
-// pub fn fmt<T>(t: TreeHandle, r: T) -> String
-//     where T: BoneRenderer {
-//     T::render(t)
-// }
-//
-// #[inline]
-// pub fn fmt_leetcode(t: TreeHandle) -> String {
-//     LeetcodeRenderer::render(t)
 // }
 //
